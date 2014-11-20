4e30747768e4208b313b1e18d27aa9921cf49248
Peter Ashcroft <pashcroft@utah.gov>
2014-11-20 08:24:12 -0700

    Committing modified file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Assignment 2 for Peter Ashcroft (November 19, 2014)
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
